d7135e381a01c3dbb41c7ee79013243cce292aa1
Henry Yang <contacthenryyang@gmail.com>
2020-10-09 00:17:09 -0700

    Set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  entry: [
+    './client/src/app.jsx'
+  ],
+  output: {
+    path: __dirname + '/client/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  devServer: {
+    contentBase: './client/dist'
+  }
+};
