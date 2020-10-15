8458851cfed7b885fad3ed9405df0a91f116b964
Henry Yang <contacthenryyang@gmail.com>
2020-10-15 16:43:14 -0700

    Plug react app into index.html, add bundle.js src

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+  <div id="appBlock">
+    test text in React app
+  </div>,
+  document.getElementById('app')
+);
