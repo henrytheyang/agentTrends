cdbaa81721cef45178b3e00e56abd15dfbded405
Henry Yang <contacthenryyang@gmail.com>
2020-11-05 00:59:51 -0800

    Add submit button

--- src/inputField.jsx
+++ src/inputField.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 
 let TextInput = (props) => {
   return (
-    <label>
-      {/* <input type="text" value={props.text}  /> */}
-      <input type="text" placeholder='Enter agent profile name' onChange={props.handleChange} onSubmit={props.handleSubmit}/>
-    </label>
+    <form>
+      <label>
+        {/* <input type="text" value={props.text}  /> */}
+        <input type="text" placeholder='Enter agent profile name' onChange={props.handleChange} onSubmit={props.handleSubmit}/>
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
   );
 }
 
