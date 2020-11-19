13d4cbe3e8f68361cece1a291e6b411dc8d630a6
Henry Yang <contacthenryyang@gmail.com>
2020-11-18 18:39:02 -0800

    Find pastSales element

--- src/domManipulation.js
+++ src/domManipulation.js
@@ -1,6 +1,8 @@
 import $ from 'jquery';
 import dummyData from './dummyData.html';
 
+// Note: switch to regex for optimization later
+
 const scanPastSales = () => {
   console.log('scanPastSales test');
   // console.log(`dummyData = ${dummyData}`)
@@ -8,7 +10,9 @@ const scanPastSales = () => {
   //   $("section").filter("#pastSales");
   // })
   $.get('https://cors-anywhere.herokuapp.com/https://www.zillow.com/profile/jessecyang')
-    .then(response => console.log(response));
+    // .then(response => console.log(response))
+    .then(response => $(response).find("section").filter("#pastSales"))
+    .then(response => console.log(response))
 };
 
 
